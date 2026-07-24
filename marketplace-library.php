08110423026d7164897456689b7fbd5753e478b9
Gilmar Aparece <aparecegilmar1@gmail.com>
2026-07-24 10:23:02 +0800

    initial commit

---
+++ marketplace-library.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The buyer's purchased-systems library now lives inside the unified
+ * marketplace dashboard at /sell.php (tab=library). This file is kept only
+ * so old bookmarks/links still land somewhere useful.
+ */
+header('Location: /sell.php?tab=library');
+exit;
