3a27c5670e8ffafada2abd506a87c9f5ceb596bf
Gilmar Aparece <aparecegilmar1@gmail.com>
2026-07-24 10:25:11 +0800

    initial commit

--- admin/logout.php
+++ admin/logout.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../includes/store.php';
-inkwell_admin_logout();
+require_once __DIR__ . '/../includes/auth.php';
+inkwell_logout_user();
 header('Location: /admin/login.php');
 exit;
