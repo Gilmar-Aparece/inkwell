7ddb5d41467fa162aad06853fe6986f3b79b3e6f
Gilmar Aparece <aparecegilmar1@gmail.com>
2026-07-24 10:39:22 +0800

    initial commit

---
+++ assets/live-previews/ewrwrwe/includes/footer.php
@@ -0,0 +1,14 @@
+<!----<footer>
+  Inkwell — 2026
+  <br>Made by Gilmar Aparece. · <a href="/admin/login.php">Admin</a>
+</footer>--->
+<!---<script src="/assets/js/protect.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/protect.js'); ?>"></script>--->
+<script src="/assets/js/search-filter.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/search-filter.js'); ?>"></script>
+<script src="/assets/js/app.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/app.js'); ?>"></script>
+<script src="/assets/js/student-profile.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/student-profile.js'); ?>"></script>
+<script src="/assets/js/teacher-profile.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/teacher-profile.js'); ?>"></script>
+<script src="/assets/js/post-profile.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/post-profile.js'); ?>"></script>
+<script src="/assets/js/post-lightbox.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/post-lightbox.js'); ?>"></script>
+<script src="/assets/js/particles.js?v=<?php echo filemtime(__DIR__ . '/../assets/js/particles.js'); ?>"></script>
+</body>
+</html>
